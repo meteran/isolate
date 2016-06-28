495b7919ebc31e9d9cb16cc645733719a74af1a7
Piotr Wojcik <piotr_to@op.pl>
2016-06-28 10:41:10 +0200

    setup and example file

--- isolate/__init__.py
+++ isolate/__init__.py
@@ -2,3 +2,9 @@
 # coding: utf-8
 
 from isolate.namespaces import NewNamespaces, JoinNamespaces, in_namespace as run_in_namespace
+from cgroups import Cgroup
+
+BYTES = Cgroup.BYTES
+KILOBYTES = Cgroup.KILOBYTES
+MEGABYTES = Cgroup.MEGABYTES
+GIGABYTES = Cgroup.GIGABYTES
--- setup.py
+++ setup.py
@@ -4,6 +4,15 @@ from distutils.core import setup
 
 setup(
     name='isolate',
+    packagges=['isolate'],
     version='0.1',
-
+    description='python module to isolated executing programs with namespaces and cgroups',
+    author=u'Piotr Wójcik',
+    author_mail='piotr_to@op.pl',
+    url='https://github.com/meteran/isolate',
+    keywords=['isolating', 'isolate', 'namespaces', 'cgroups', 'cgroup', 'namespace'],
+    classifiers=[],
+    install_requires=[
+        'cgroupspy',
+    ]
 )
